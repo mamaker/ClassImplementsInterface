8b33fa2f537b89858f4ef6730f7c1ce4bac97fca
mamaker <madhuvasudevan@hotmail.com>
2015-05-09 12:00:06 -0500

    Done

---
+++ ClassImplementsInterface/Bee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassImplementsInterface
+{
+    class Bee : IstingPatrol
+    {
+        public int AlertLevel { get; private set; }
+        public int StingerLength { get; set; }
+        public bool LookForEnemies() {
+            return true;
+        }
+        public int SharpenStinger(int length) {
+            return 0;
+        }
+    }
+}
---
+++ ClassImplementsInterface/IstingPatrol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassImplementsInterface
+{
+    interface IstingPatrol
+    {
+        int AlertLevel { get; }
+        int StingerLength { get; set; }
+        bool LookForEnemies();
+        int SharpenStinger(int length);
+    }
+}
